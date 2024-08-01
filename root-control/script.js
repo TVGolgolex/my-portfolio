7c557cb7ad41cf72234b377b73179737e8593042
Pascal <75942385+TVGolgolex@users.noreply.github.com>
2024-08-01 20:19:30 +0200

    added root-control

---
+++ root-control/script.js
@@ -0,0 +1,30 @@
+let menuIcon = document.querySelector('#menuIcon');
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-x");
+    navbar.classList.toggle("active");
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    let header = document.querySelector(".header");
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 170;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    menuIcon.classList.remove("fa-x");
+    navbar.classList.remove("active");
+};
